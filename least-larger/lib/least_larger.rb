53521fd58aa1085e1d5335ca4fe1d57929ae8624
Mark McLeod <markmcleod45@gmail.com>
2022-03-08 16:14:24 +0000

    Solution with tests

--- least-larger/lib/least_larger.rb
+++ least-larger/lib/least_larger.rb
@@ -1,3 +1,11 @@
 def least_larger(array, index)
-  p array[index]
+  sorted_array = array.sort
+  array_index_value = array[index]
+  if sorted_array.find_index(array_index_value + 1) == nil
+    -1
+  else
+    index_position_for_next_largest_value = sorted_array.find_index(array_index_value + 1)
+    next_largest_value = sorted_array[index_position_for_next_largest_value]
+    array.find_index(next_largest_value)
+  end
 end
--- least-larger/spec/least_larger_spec.rb
+++ least-larger/spec/least_larger_spec.rb
@@ -2,6 +2,8 @@ require 'least_larger'
 
 describe '#least_larger' do
   it 'returns the value of the index position provided' do
-    expect(least_larger([4, 1, 3, 5, 6], 0)).to eq 4
+    expect(least_larger([4, 1, 3, 5, 6], 0)).to eq 3
+    expect(least_larger([1, 3, 5, 2, 4], 0)).to eq 3
+    expect(least_larger([4, 1, 3, 5, 6], 4)).to eq -1
   end
 end
