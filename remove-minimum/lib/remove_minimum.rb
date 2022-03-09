56724ee453abe3909c755544c1e05746c9c2f78e
Mark McLeod <markmcleod45@gmail.com>
2022-03-09 14:42:35 +0000

    Solution with tests

--- remove-minimum/lib/remove_minimum.rb
+++ remove-minimum/lib/remove_minimum.rb
@@ -1,4 +1,8 @@
-def remove_minimum(array)
-  array.delete(array.min)
-  p array
+def remove_minimum(numbers)
+  if numbers == []
+    numbers
+  else
+  new_numbers = numbers.dup
+  new_numbers.delete_at(new_numbers.index(new_numbers.min))
+  end
 end
--- remove-minimum/spec/remove_minimum_spec.rb
+++ remove-minimum/spec/remove_minimum_spec.rb
@@ -3,9 +3,9 @@ require 'remove_minimum'
 describe '#remove_minimum' do
   it 'removes the smallest value from an array' do
     expect(remove_minimum([1, 2, 3, 4, 5])).to eq [2, 3, 4, 5]
+    expect(remove_minimum([5, 3, 2, 1, 4])).to eq [5, 3, 2, 4]
+    expect(remove_minimum([2,2,1,2,1])).to eq [2,2,2,1]
+    expect(remove_minimum([])).to eq []
   end
 
-  it 'removes the smallest value from an array' do
-    expect(remove_minimum([5, 3, 2, 1, 4])).to eq [5, 3, 2, 4]
-  end
 end
