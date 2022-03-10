520c7af23a15bf01553d46a10f07eb15d8aa3d84
Mark McLeod <markmcleod45@gmail.com>
2022-03-10 10:35:51 +0000

    Solution with tests

--- sum-arguments/spec/sum_spec.rb
+++ sum-arguments/spec/sum_spec.rb
@@ -4,4 +4,12 @@ describe '#sum' do
   it 'adds two arguments together' do
     expect(sum(8, 2)).to eq 10
   end
+
+  it 'adds three arguments together' do
+    expect(sum(8, 2, 5)).to eq 15
+  end
+
+  it 'adds five arguments together' do
+    expect(sum(8, 2, 5, 3, 2)).to eq 20
+  end
 end
