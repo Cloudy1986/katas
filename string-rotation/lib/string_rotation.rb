6c26437e834dcf74cfce01fc6c5d074a958bdfb7
Mark McLeod <markmcleod45@gmail.com>
2022-03-11 12:46:17 +0000

    Solution with tests

--- string-rotation/lib/string_rotation.rb
+++ string-rotation/lib/string_rotation.rb
@@ -1,4 +1,15 @@
 def shifted_diff(first, second)
-  first_letter = first[0]
-  rotation = second.index(first_letter)
+  array_first = first.split("")
+  array_second = second.split("")
+  count = 0
+  if first == second
+    0
+  else
+    loop do
+      array_first.rotate!(-1)
+      count +=1
+      break if array_first == array_second || count >= array_first.size
+    end
+    count < array_first.size ? count : -1
+  end
 end
--- string-rotation/spec/string_rotation_spec.rb
+++ string-rotation/spec/string_rotation_spec.rb
@@ -3,9 +3,16 @@ require 'string_rotation'
 describe '#shifted_diff' do
   it 'calculates the rotations needed for first input to equal second input' do
     expect(shifted_diff("coffee", "eecoff")).to eq 2
+    expect(shifted_diff("eecoff", "coffee")).to eq 4
+    expect(shifted_diff("isn't", "'tisn")).to eq 2
   end
 
-  it 'calculates the rotations needed for first input to equal second input' do
-    expect(shifted_diff("eecoff", "coffee")).to eq 4
+  it 'returns 0 when the first input and second input are the same' do
+    expect(shifted_diff("Esham", "Esham")).to eq 0
+  end
+
+  it 'returns -1 when the first input and second input are not valid rotations' do
+    expect(shifted_diff("moose", "Moose")).to eq -1
+    expect(shifted_diff("dog", "god")).to eq -1
   end
 end
