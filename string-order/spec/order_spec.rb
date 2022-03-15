94b98519df68b809b3ad867de454fd7eafaad444
Mark McLeod <markmcleod45@gmail.com>
2022-03-15 17:18:37 +0000

    Add another test

--- string-order/spec/order_spec.rb
+++ string-order/spec/order_spec.rb
@@ -12,12 +12,8 @@ describe '#order' do
   it 'reorders a string by the number each word contains' do
     expect(order('is2 Thi1s T4est 3a')).to eq 'Thi1s is2 3a T4est'
   end
+
+  it 'reorders a string by the number each word contains' do
+    expect(order('4of Fo1r pe6ople g3ood th5e the2')).to eq 'Fo1r the2 g3ood 4of th5e pe6ople'
+  end
 end
-
-# Test plan
-
-# if empty string then return empty string
-
-# 2 words
-
-# 4 words
