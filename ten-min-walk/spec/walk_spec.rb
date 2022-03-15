b48712c16cf2e09eff16cffc9a655fa65d9130d3
Mark McLeod <markmcleod45@gmail.com>
2022-03-15 16:48:19 +0000

    Update tests

--- ten-min-walk/spec/walk_spec.rb
+++ ten-min-walk/spec/walk_spec.rb
@@ -6,19 +6,15 @@ describe 'is_valid_walk' do
     expect(is_valid_walk(['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'n', 's', 'w', 'e'])).to eq false
   end
 
-  it 'returns true if the walk is ten mins' do
-    expect(is_valid_walk(['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'n', 's'])).to eq true
+  it 'returns false if the walk does not return to the start' do
+    expect(is_valid_walk(['n', 'n', 'w', 'e', 'n', 'n', 'w', 'e', 'n', 'e'])).to eq false
+  end
+
+  it 'returns false if the walk is ten mins but does not return to the start' do
+    expect(is_valid_walk(['n', 's', 's', 'e', 'n', 's', 's', 'e', 'n', 's'])).to eq false
   end
 
   it 'returns true if the walk is ten mins and returns to the starting position' do
     expect(is_valid_walk(['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'n', 's'])).to eq true
   end
 end
-
-# Test approach
-
-# Check if walk is 10 mins, return either true or false
-
-# Check if return to start position, return either true or false
-
-# Check both are true, return true or false
