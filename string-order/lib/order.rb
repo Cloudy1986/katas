8b67cd857d6ecc81e66676377bd5c922dc45f34c
Mark McLeod <markmcleod45@gmail.com>
2022-03-15 16:58:45 +0000

    Test for empty string passing

---
+++ string-order/lib/order.rb
@@ -0,0 +1,3 @@
+def order(words)
+  words
+end
---
+++ string-order/spec/order_spec.rb
@@ -0,0 +1,15 @@
+require 'order'
+
+describe '#order' do
+  it 'returns an empty string if passed an empty string' do
+    expect(order('')).to eq ''
+  end
+end
+
+# Test plan
+
+# if empty string then return empty string
+
+# 2 words
+
+# 4 words
