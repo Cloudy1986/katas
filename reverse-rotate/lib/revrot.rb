18832ca7b70c1f605ffcd9c88ab43738173600d3
Mark McLeod <markmcleod45@gmail.com>
2022-03-16 14:48:05 +0000

    Tests passing

--- reverse-rotate/lib/revrot.rb
+++ reverse-rotate/lib/revrot.rb
@@ -1,4 +1,7 @@
 def revrot(str, sz)
-  "" if str == "" || sz <= 0 || sz > str.length
-
+  if str == "" || sz <= 0 || sz > str.length
+    ""
+  else
+  chunks_array = str.scan(/.{1,3}/).select { |chunk| chunk.length == sz }
+  end
 end
--- reverse-rotate/spec/revrot_spec.rb
+++ reverse-rotate/spec/revrot_spec.rb
@@ -12,13 +12,13 @@ describe '#revrot' do
   it 'returns an empty string if sz is greater than the length of str' do
     expect(revrot("1234", 5)).to eq ""
   end
+
+  it 'chunks the string into substrings with a length of sz and deletes a chunk if it is not the same length as sz' do
+    expect(revrot("12345678", 3)).to eq ["123", "456"]
+  end
 end
 
 # High level logic
-
-# 2. If they are valid then
-# else
-# cut the chunk into substrings of length sz
 
 # 3. Check if the sum of the cubes of each digit in the chunk is divisible by two ( % 2 == 0)
 # if yes then reverse the chunk
