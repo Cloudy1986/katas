403a7261ee88c37d1199496403427f37d1b9ef62
Mark McLeod <markmcleod45@gmail.com>
2022-03-17 17:28:06 +0000

    Tests passing

--- bouncing-balls/lib/bouncing_ball.rb
+++ bouncing-balls/lib/bouncing_ball.rb
@@ -1,11 +1,22 @@
 def bouncing_ball(h, bounce, window)
-  p h
-  p bounce
-  p window
+  p "h at the start is #{h}"
+  p "bounce at the start is #{bounce}"
+  p "window at the start is #{window}"
+  ball_viewed = 1
+  p "ball_viewed at the start is #{ball_viewed}"
   
   if h <= 0 || bounce <= 0 || bounce >= 1 || window >= h
     -1
   else
-    1
+    h = h * bounce
+    while h > window
+      ball_viewed += 2
+      h = h * bounce
+      p h
+      p ball_viewed
+    end
+    ball_viewed
   end
 end
+
+# Calculate how many times see the ball for first bounce
--- bouncing-balls/spec/bouncing_ball_spec.rb
+++ bouncing-balls/spec/bouncing_ball_spec.rb
@@ -7,12 +7,36 @@ describe '#bouncing_ball' do
     expect(bouncing_ball(5.0, 0.5, 10.5)).to eq -1
   end
 
-  it 'returns an integer if the conditions for the experiment are met' do
-    expect(bouncing_ball(3.0, 0.66, 1.5)).to eq 1
+  it 'returns the times the ball is viewed for one bounce if the conditions for the experiment are met' do
+    expect(bouncing_ball(3.0, 0.66, 1.5)).to eq 3
+  end
+
+  it 'returns the times the ball is viewed for two bounces if the conditions for the experiment are met' do
+    expect(bouncing_ball(3.0, 0.66, 1.1)).to eq 5
   end
 end
 
 #Logic
 
-# if h > 0 && bounce > 0 && bounce < 1 && window < h
-# return -1
+# Conditions are met...
+
+# Start
+# ball starts at h
+# +1 on way down
+
+# First bounce
+# bounces to h * bounce
+# +1 if h * bounce > window and +1 on way down again
+# break if h * bounce is <= window
+
+# Second bounce
+# bounces to h * bounce * bounce
+# +1 if h * bounce * bounce > window and +1 on way down again
+# break if h * bounce * bounce <= window
+
+# Third bounce
+# bounces to h * bounce * bounce * bounce
+# +1 if h * bounce * bounce * bounce > window and +1 on way down again
+# break if h * bounce * bounce * bounce <= window
+
+
