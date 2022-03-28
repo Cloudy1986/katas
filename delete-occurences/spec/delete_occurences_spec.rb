bb44b95dc7823d63ee0b8ffc28dae99dc1944aaf
Mark McLeod <markmcleod45@gmail.com>
2022-03-28 16:34:12 +0100

    Fix spaces in arrays

--- delete-occurences/spec/delete_occurences_spec.rb
+++ delete-occurences/spec/delete_occurences_spec.rb
@@ -1,11 +1,11 @@
 require 'delete_occurences'
 
 describe '#delete_occurences' do
-  it 'removes duplicates from the array when passed the argument 1' do
-    expect(delete_occurences([20,37,20, 40, 21, 40], 1)).to eq [20,37, 40, 21]
+  it 'removes duplicates from an array when passed the argument 1' do
+    expect(delete_occurences([20,37,20,40,21,40], 1)).to eq [20,37,40,21]
   end
 
-  it 'removes the third occurence of a value from an arry when passed the argument 2' do
+  it 'removes the third occurence of a value from an array when passed the argument 2' do
     expect(delete_occurences([1,2,3,1,2,1,2,3], 2)).to eq [1,2,3,1,2,3]
   end
 end
