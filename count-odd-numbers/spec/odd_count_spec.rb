5aafcadbfbc9852aecaabee6730948db782bd7a5
Mark McLeod <markmcleod45@gmail.com>
2022-03-28 16:27:57 +0100

    Fix typo

--- count-odd-numbers/spec/odd_count_spec.rb
+++ count-odd-numbers/spec/odd_count_spec.rb
@@ -1,7 +1,7 @@
 require 'odd_count'
 
 describe '#odd_count' do
-  it 'counts the number of odd number below the given input' do
+  it 'counts the number of odd numbers below the given input' do
     expect(odd_count(7)).to eq 3
     expect(odd_count(15)).to eq 7
     expect(odd_count(15023)).to eq 7511
