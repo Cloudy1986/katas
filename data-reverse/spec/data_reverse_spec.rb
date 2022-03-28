bd370f3177954435579ca49ff7753a2c0e5b20bd
Mark McLeod <markmcleod45@gmail.com>
2022-03-28 16:32:38 +0100

    Update test description

--- data-reverse/spec/data_reverse_spec.rb
+++ data-reverse/spec/data_reverse_spec.rb
@@ -1,7 +1,7 @@
 require 'data_reverse'
 
 describe '#data_reverse' do
-  it 'splits an array into 8 character long elements' do
+  it 'splits an array into 8 character long chunks, reverses the index position of each chunk, then flattens the chunks back into one array' do
     expect(data_reverse([1,2,3,4,5,6,7,8,0,1,0,1,0,1,0,1,10,20,30,40,50,60,70,80])).to eq [10,20,30,40,50,60,70,80,0,1,0,1,0,1,0,1,1,2,3,4,5,6,7,8]
   end
 end
