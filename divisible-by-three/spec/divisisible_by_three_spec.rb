d4bda2da3104445d3bcc78544539ed6a3159918d
Mark McLeod <markmcleod45@gmail.com>
2022-03-28 16:36:08 +0100

    Update test description

--- divisible-by-three/spec/divisisible_by_three_spec.rb
+++ divisible-by-three/spec/divisisible_by_three_spec.rb
@@ -1,11 +1,11 @@
 require 'divisible_by_three'
 
 describe '#divisible_by_three' do
-  it 'returns true if the number is divisible by three' do
+  it 'returns true if the sum of the digits in the string are divisible by three' do
     expect(divisible_by_three('123')).to eq true
   end
 
-  it 'returns false if the number is not divisible by three' do
+  it 'returns false if the sum of the digits in the string are not divisible by three' do
     expect(divisible_by_three('1234')).to eq false
   end
 end
