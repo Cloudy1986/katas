5d251ec7d5e5a3048e189446fd703cfcf9494a1e
Mark McLeod <markmcleod45@gmail.com>
2022-03-28 16:26:40 +0100

    Update descriptions of tests

--- arabian-string/spec/arabian_string_spec.rb
+++ arabian-string/spec/arabian_string_spec.rb
@@ -6,24 +6,24 @@ describe '#camelize' do
     expect(camelize("hAPPiness")).to eq "Happiness"
   end
 
-  it 'capitalizes each word and joins them for a two word string with spaces' do
+  it 'capitalizes each word and joins them together when passed a two word string with spaces' do
     expect(camelize("tired today")).to eq "TiredToday"
     expect(camelize("tIRed TodAy 2021")).to eq "TiredToday2021"
   end
 
-  it 'capitalizes multi-word string seperated by symbols' do
+  it 'capitalizes a multi-word string that is seperated by symbols' do
     expect(camelize("your-NaMe-here")).to eq "YourNameHere"
   end
 
-  it 'capitalizes multi-word string seperated by symbols' do
+  it 'capitalizes a multi-word string that is seperated by symbols' do
     expect(camelize("Rugby:Club:2013")).to eq "RugbyClub2013"
   end
 
-  it 'capitalizes multi-word string seperated by symbols' do
+  it 'capitalizes a multi-word string that is seperated by => and spaces' do
     expect(camelize("'quOted' => 'What'")).to eq "QuotedWhat"
   end
 
-  it 'capitalizes multi-word string seperated by symbols' do
+  it 'capitalizes a multi-word string seperated by _ and -' do
     expect(camelize("Arabian_String-Test")).to eq "ArabianStringTest"
   end
 end
