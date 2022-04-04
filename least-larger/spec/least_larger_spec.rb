53f3c4c36f17e139062b104c4fac3fe212f351eb
Mark McLeod <markmcleod45@gmail.com>
2022-04-04 17:17:09 +0100

    Update test description

--- least-larger/spec/least_larger_spec.rb
+++ least-larger/spec/least_larger_spec.rb
@@ -1,7 +1,7 @@
 require 'least_larger'
 
 describe '#least_larger' do
-  it 'returns the value of the index position provided' do
+  it 'returns the index position of the least larger number than the value of index position provided' do
     expect(least_larger([4, 1, 3, 5, 6], 0)).to eq 3
     expect(least_larger([1, 3, 5, 2, 4], 0)).to eq 3
     expect(least_larger([4, 1, 3, 5, 6], 4)).to eq -1
