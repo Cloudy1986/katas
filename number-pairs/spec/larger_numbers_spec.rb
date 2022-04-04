cc0a66aa5301d2f3a7f1ceb828db1e9db2cb82fe
Mark McLeod <markmcleod45@gmail.com>
2022-04-04 17:19:26 +0100

    Update test descriptions

--- number-pairs/spec/larger_numbers_spec.rb
+++ number-pairs/spec/larger_numbers_spec.rb
@@ -1,15 +1,15 @@
 require 'larger_numbers'
 
 describe '#larger_numbers' do
-  it 'returns an array with the largest integer from the two input arrays for one element' do
+  it 'returns an array with the largest integer from the two input arrays' do
     expect(get_larger_numbers([13], [23])).to eq [23]
   end
 
-  it 'returns an array with the largest integers from the two input arrays for two elements' do
+  it 'returns an array with the largest two integers from the two input arrays' do
     expect(get_larger_numbers([13, 64], [23, 14])).to eq [23, 64]
   end
 
-  it 'returns an array with the largest integers from the two input arrays for 5 elements' do
+  it 'returns an array with the largest five integers from the two input arrays' do
     expect(get_larger_numbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80])).to eq [23, 64, 53, 17, 88]
   end
 end
