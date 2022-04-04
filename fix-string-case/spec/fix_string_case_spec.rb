899514bbd4e2552640d18c28ed7c6501247fb3d8
Mark McLeod <markmcleod45@gmail.com>
2022-04-04 17:12:50 +0100

    Fix test descriptions

--- fix-string-case/spec/fix_string_case_spec.rb
+++ fix-string-case/spec/fix_string_case_spec.rb
@@ -1,11 +1,11 @@
 require 'fix_string_case'
 
 describe '#solve' do
-  it 'returns all lowercase when input it all lowercase' do
+  it 'returns all lowercase when input is all lowercase' do
     expect(solve('code')).to eq 'code'
   end
 
-  it 'returns all uppercase when input it all uppercase' do
+  it 'returns all uppercase when input is all uppercase' do
     expect(solve('CODE')).to eq 'CODE'
   end
 
@@ -13,7 +13,7 @@ describe '#solve' do
     expect(solve('COde')).to eq 'code'
   end
 
-  it 'returns all lowercase when input is equally lowercase and uppercase' do
+  it 'returns all uppercase when input is more uppercase than lowercase' do
     expect(solve('CODe')).to eq 'CODE'
   end
 
