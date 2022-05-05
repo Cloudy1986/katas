6e2833502ed1a22206db8a4392a7d4803dd9a45a
Mark McLeod <markmcleod45@gmail.com>
2022-05-05 11:14:05 +0100

    Next two tests

--- plural/spec/plural_spec.rb
+++ plural/spec/plural_spec.rb
@@ -2,6 +2,14 @@ require 'plural'
 
 describe '#plural' do
   it 'returns false when passed 1' do
-  expect(plural(1)).to eq false
+    expect(plural(1)).to eq false
+  end
+
+  it 'returns true when passed 0' do
+    expect(plural(0)).to eq true
+  end
+
+  it 'returns true when passed 5' do
+    expect(plural(5)).to eq true
   end
 end
