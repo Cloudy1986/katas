00e62922e3b074cbf4c5705f615fd2e50e188aea
Mark McLeod <markmcleod45@gmail.com>
2022-05-09 15:29:25 +0100

    Tests passing

--- bingo/lib/bingo.rb
+++ bingo/lib/bingo.rb
@@ -1,3 +1,3 @@
 def bingo(arr)
-  arr.include?(2) ? "Win" : "Lose"
+  arr.include?(2) && arr.include?(7) && arr.include?(9) && arr.include?(14) && arr.include?(15) ? "WIN" : "LOSE"
 end
--- bingo/spec/bingo_spec.rb
+++ bingo/spec/bingo_spec.rb
@@ -1,7 +1,8 @@
 require 'bingo'
 
 describe '#bingo' do
-  it 'checks if the input array contains 2' do
-    expect(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq "Win"
+  it 'checks if the input array contains 2, 7, 9, 14, 15' do
+    expect(bingo([1, 2, 3, 4, 5, 7, 8, 9, 14, 15])).to eq "WIN"
+    expect(bingo([1, 2, 3, 4, 5, 6, 8, 9, 10, 11])).to eq "LOSE"
   end
 end
