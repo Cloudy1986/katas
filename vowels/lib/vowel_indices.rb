efecc36374f22577013ed8b09ef38cac81166e71
Mark McLeod <markmcleod45@gmail.com>
2022-05-11 11:58:53 +0100

    Completed solution

--- vowels/lib/vowel_indices.rb
+++ vowels/lib/vowel_indices.rb
@@ -1,7 +1,5 @@
 def vowel_indices(word)
-  zero_indexed_array = word.downcase.split("").map.with_index { |letter, index| index if ["a", "e", "i", "o", "u"].include?(letter) }
-  zero_indexed_array.delete(nil)
-  p zero_indexed_array
-  adjusted_indexed_array = zero_indexed_array.map { |number| number + 1}
-  p adjusted_indexed_array
+  vowel_index_positions = word.downcase.split("").map.with_index { |letter, index| index if ["a", "e", "i", "o", "u", "y"].include?(letter) }
+  vowel_index_positions.delete(nil)
+  adjusted_index_positions = vowel_index_positions.map { |number| number + 1}
 end
--- vowels/spec/vowel_indices_spec.rb
+++ vowels/spec/vowel_indices_spec.rb
@@ -2,7 +2,7 @@ require 'vowel_indices'
 
 describe '#vowel_indices' do
   it 'returns an empty array for a string with no vowels' do
-    expect(vowel_indices("zXyD")).to eq []
+    expect(vowel_indices("zXpD")).to eq []
   end
 
   it 'returns the not zero indexed position for a string with one vowel' do
@@ -12,6 +12,8 @@ describe '#vowel_indices' do
   it 'returns the not zero indexed position for a string with two vowels' do
     expect(vowel_indices("AtezGv")).to eq [1, 3]
   end
-end
 
-# a e i o u y
+  it 'returns an empty array with multiple vowels' do
+    expect(vowel_indices("bialy")).to eq [2, 3, 5]
+  end
+end
