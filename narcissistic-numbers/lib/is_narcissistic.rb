7e48b1762b9b92a13015f89c0a564cabd789deb5
Mark McLeod <markmcleod45@gmail.com>
2022-05-11 15:52:26 +0100

    Refactor

--- narcissistic-numbers/lib/is_narcissistic.rb
+++ narcissistic-numbers/lib/is_narcissistic.rb
@@ -1,6 +1,4 @@
 def is_narcissistic(number)
-  p length = number.digits.length
-  p digits = number.digits.reverse.map { |digit| digit ** length }.sum
-
-  p (number.digits.reverse.map { |digit| digit ** length }.sum) == number
+  length = number.digits.length
+  (number.digits.reverse.map { |digit| digit ** length }.sum) == number
 end
--- narcissistic-numbers/spec/is_narcissistic_spec.rb
+++ narcissistic-numbers/spec/is_narcissistic_spec.rb
@@ -3,9 +3,13 @@ require 'is_narcissistic'
 describe '#is_narcissistic' do
   it 'returns true of the input value is narcissistic' do
     expect(is_narcissistic(153)).to eq true
+    expect(is_narcissistic(1)).to eq true
+    expect(is_narcissistic(8208)).to eq true
   end
 
   it 'returns false if the input value is not narcissistic' do
     expect(is_narcissistic(324)).to eq false
+    expect(is_narcissistic(4328)).to eq false
+    expect(is_narcissistic(3248)).to eq false
   end
 end
