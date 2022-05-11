2932e6dc564ca61bcd4e38bd1d791f926a8d655c
Mark McLeod <markmcleod45@gmail.com>
2022-05-11 15:10:55 +0100

    Completed solution

--- longest-word/lib/longest.rb
+++ longest-word/lib/longest.rb
@@ -1,3 +1,6 @@
 def longest(words)
-  words.size
+  words.sort_by(&:length).last.size
 end
+
+# The code above is doing the same as the line below:
+# words.sort_by { |word| word.length }.last.size
--- longest-word/spec/longest_spec.rb
+++ longest-word/spec/longest_spec.rb
@@ -1,7 +1,7 @@
 require 'longest'
 
 describe '#longest' do
-  it 'returns the length of a string' do
-    expect(longest("complex")).to eq 7
+  it 'returns the length of the longest string in the input array' do
+    expect(longest(['simple', 'is', 'better', 'than', 'complex'])).to eq 7
   end
 end
