436bb55572e027930ed2aa1e3b0e2844036393ae
Mark McLeod <markmcleod45@gmail.com>
2022-05-11 12:06:37 +0100

    Refactor

--- vowels/lib/vowel_indices.rb
+++ vowels/lib/vowel_indices.rb
@@ -1,5 +1,3 @@
 def vowel_indices(word)
-  vowel_index_positions = word.downcase.split("").map.with_index { |letter, index| index if ["a", "e", "i", "o", "u", "y"].include?(letter) }
-  vowel_index_positions.delete(nil)
-  adjusted_index_positions = vowel_index_positions.map { |number| number + 1}
+  word.downcase.split("").map.with_index(1) { |letter, index| index if ["a", "e", "i", "o", "u", "y"].include?(letter) }.compact
 end
