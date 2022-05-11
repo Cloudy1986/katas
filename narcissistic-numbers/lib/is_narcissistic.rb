87346b512c87908a77be4b45f9800b65608b6b49
Mark McLeod <markmcleod45@gmail.com>
2022-05-11 15:58:20 +0100

    Remove unneccessary brackets

--- narcissistic-numbers/lib/is_narcissistic.rb
+++ narcissistic-numbers/lib/is_narcissistic.rb
@@ -1,4 +1,4 @@
 def is_narcissistic(number)
   length = number.digits.length
-  (number.digits.reverse.map { |digit| digit ** length }.sum) == number
+  number.digits.reverse.map { |digit| digit ** length }.sum == number
 end
