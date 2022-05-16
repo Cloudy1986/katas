d00b1e4ea493e5aa57ea0266c493b5ccc1b2297e
Mark McLeod <markmcleod45@gmail.com>
2022-05-16 11:18:57 +0100

    Completed solution

--- pairs/lib/pairs.rb
+++ pairs/lib/pairs.rb
@@ -1,3 +1,10 @@
-def pairs(arr)
-  arr
+def pairs(numbers)
+  if numbers == [] || numbers.length == 1
+    0
+  else
+    group_of_numbers = numbers.sort.group_by { |number| number }.values
+    group_of_numbers.map { |group| group.pop if group.length % 2 != 0 }.compact
+    group_of_numbers.delete([])
+    number_of_pairs = group_of_numbers.map { |group| group.length }.sum / 2
+  end
 end
--- pairs/spec/pairs_spec.rb
+++ pairs/spec/pairs_spec.rb
@@ -1,7 +1,15 @@
 require 'pairs'
 
 describe '#pairs' do
-  it 'returns the input array' do
-    expect(pairs([1, 2, 5, 6, 5, 2])).to eq [1, 2, 5, 6, 5, 2]
+  it 'returns the number of pairs in the input array' do
+    expect(pairs([1, 2, 5, 6, 5, 2, 1, 1, 7, 9, 5, 5])).to eq 4
+  end
+
+  it 'returns zero when the input array is empty' do
+    expect(pairs([])).to eq 0
+  end
+
+  it 'returns zero when the input array contains only one value' do
+    expect(pairs([10])).to eq 0
   end
 end
